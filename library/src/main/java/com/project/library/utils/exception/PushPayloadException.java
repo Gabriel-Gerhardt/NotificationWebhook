3d7bbed431a9605ed632aec39ebd4bb0685aa7d9
gabriel-gerhardt <gabrielgerhardt27@gmail.com>
2026-01-13 14:19:31 -0300

    .

--- library/src/main/java/com/project/library/utils/exception/PushPayloadException.java
+++ library/src/main/java/com/project/library/utils/exception/PushPayloadException.java
@@ -1,17 +1,6 @@
 package com.project.library.utils.exception;
 
-import lombok.Getter;
-
-@Getter
-public class PushPayloadException {
-
-    private final String message;
-    private final RuntimeException cause;
-
-    public PushPayloadException(String message, RuntimeException cause) {
-        this.message = message;
-        this.cause = cause;
-    }
+public record PushPayloadException(String message, RuntimeException cause) {
 
     public RuntimeException toRuntimeException() {
         return new RuntimeException(message, cause);
